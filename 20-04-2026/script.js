f2f01bc697e9e43613d2bf028e582f3989bdbb0e
Jhoel170 <jhoel.beltran@epn.edu.ec>
2026-04-22 16:02:06 -0500

    Xd3

--- 20-04-2026/script.js
+++ 20-04-2026/script.js
@@ -12,9 +12,50 @@ saludar();*/
 
 insertarEstudiante();*/
 
-const agregarEstudiante = () => {
+/*const estudiante = {id: 1, nombre: "Jhoel Beltrán"};*/
+
+const lstEstudiante = [
+    {id: 1, nombre: "Jhoel"},
+    {id: 2, nombre: "Oscar"},
+    {id: 3, nombre: "Daniel"}
+]
+
+const renderizarListaEstudiantes = () => {
+    const divContenedor = document.getElementById("contenedor");
+    divContenedor.innerText = "";
+    lstEstudiante.forEach((estudiante) => {
+        const itemEstudiante = document.createElement("p");
+        itemEstudiante.innerText = estudiante.nombre;
+        divContenedor.appendChild(itemEstudiante);
+    })
+}
+
+
+
+/*lstEstudiante.forEach((estudiante) => {
+    console.log(estudiante);
+})*/
+
+/*const agregarEstudiante = () => {
+    const divContenedor = document.getElementById("contenedor");
+    const itemEstudiante = document.createElement("p");
+    itemEstudiante.innerText = estudiante.nombre;
+    divContenedor.appendChild(itemEstudiante);
+}*/
+
+/*const agregarEstudiante = () => {
+
     const divContenedor = document.getElementById("contenedor");
     const itemEstudiante = document.createElement("p");
     itemEstudiante.innerText = "Jhoel";
     divContenedor.appendChild(itemEstudiante);
+}*/
+
+const insertarEstudiante = (event) => {
+    event.preventDefault();
+    const nombreEstudiante = document.getElementById("txtEstudiante").value;
+    lstEstudiante.push({id: 0, nombre: nombreEstudiante});
+    renderizarListaEstudiante();
 }
+
+renderizarListaEstudiantes();
